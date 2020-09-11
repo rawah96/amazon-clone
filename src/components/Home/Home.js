7bd16b05aac6693e8d42d71c50636e7d1c493171
rawah <rawah@pdx.edu>
2020-09-11 00:05:40 -0700

    added products

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,89 @@
 import React from 'react'
 import './Home.css'
 import astro from './ast.jpg'
+import Product from '../Products/Product'
+import short from './short.jpg'
+import short2 from './short2.jpg'
+import astro2 from './astro3.jpg'
 function Home() {
     return (
         <div className="home">
             <img src={astro} alt="" className="home-img" />
+            <div className="home-row">
+            <Product 
+            id="SEVEN-STARS"
+            title="No Tax Seven Stars Tshirt" 
+            price={39.99}
+            rating={7}
+            image={astro2}
+            />
+
+            <Product 
+            id="DANCE-MONKEY"
+            title="Dance Monkey Tshirt"
+            price={39.99}
+            rating={4.8}
+            image={astro2}
+            />
+
+            <Product 
+            id="SAUDI-ARABIA"
+            title="Saudi Arabia Short"
+            price={32.99}
+            rating={5}
+            image={short}
+            />
+
+            <Product 
+            id="DOUBT-ME"
+            title="Doubted Me Short"
+            price={32.99}
+            rating={5}
+            image={short2}
+            />
+            </div>
+                <div className="home-row">
+                <Product 
+                id="SEVEN-STARS"
+                title="No Tax Seven Stars Tshirt" 
+                price={39.99}
+                rating={7}
+                image={astro2}
+                />
+                <Product 
+                id="DANCE-MONKEY"
+                title="Dance Monkey Tshirt"
+                price={39.99}
+                rating={4.8}
+                image={astro2}
+                />
+
+                <Product 
+                id="SAUDI-ARABIA"
+                title="Saudi Arabia Short"
+                price={32.99}
+                rating={5}
+                image={short}
+                />
+            </div>
+
+            <div className="home-row">
+                <Product 
+                id="SEVEN-STARS"
+                title="No Tax Seven Stars Tshirt" 
+                price={39.99}
+                rating={7}
+                image={astro2}
+                />
+
+                <Product 
+                id="DANCE-MONKEY"
+                title="Dance Monkey Tshirt"
+                price={39.99}
+                rating={4.8}
+                image={astro2}
+                />
+            </div>
         </div>
     )
 }
---
+++ src/components/Products/Product.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Product.css'
+
+function Product({id, title, price, rating, image}) {
+    return (
+        <div className="product">
+            <div className="product-info">
+            <p>{title}</p>
+            <p className="product-price">
+                <small>$</small>
+                <strong>{price}</strong>
+            </p>
+
+            <div className="stars">
+                {rating} stars
+            </div>
+            </div>
+
+            <img src={image} alt="" />
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
