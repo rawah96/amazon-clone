4a9de9581a198259de4b1492609194b2b6fe21cb
rawah <rawah@pdx.edu>
2020-09-11 20:17:47 -0700

    Updates

---
+++ precache-manifest.580e71f5bb6f67d5c770764c0519572b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc710ead2d46afa0285b60ed14f35df",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "51e27ca908aeaeb4a4ee",
+    "url": "/amazon-clone/static/css/main.10deb5ae.chunk.css"
+  },
+  {
+    "revision": "56dff7c93fe355c769b9",
+    "url": "/amazon-clone/static/js/2.ee7ce646.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.ee7ce646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e27ca908aeaeb4a4ee",
+    "url": "/amazon-clone/static/js/main.4625715f.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  },
+  {
+    "revision": "7451f65b8ffd3f20732b3ac43e21942a",
+    "url": "/amazon-clone/static/media/amazon.7451f65b.png"
+  },
+  {
+    "revision": "c2c56ff1cb0213dbf2ef66488f8a731f",
+    "url": "/amazon-clone/static/media/ast.c2c56ff1.jpg"
+  },
+  {
+    "revision": "cb936b2bc5586ed6bf88d7d5a7c51f0e",
+    "url": "/amazon-clone/static/media/astro3.cb936b2b.jpg"
+  },
+  {
+    "revision": "726f360b09c3015583df52ca178ce514",
+    "url": "/amazon-clone/static/media/mask.726f360b.jpg"
+  },
+  {
+    "revision": "4141f1e44dc182444c0c6db3a703441c",
+    "url": "/amazon-clone/static/media/short.4141f1e4.jpg"
+  },
+  {
+    "revision": "bae5b70680b4aa8787022789eae1aaa4",
+    "url": "/amazon-clone/static/media/short2.bae5b706.jpg"
+  }
+]);
